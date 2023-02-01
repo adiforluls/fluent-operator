bf09ac1df6c16fd221605fda0a888393ac7da22a
adiforluls <aditya.b.bharadwaj@oracle.com>
2023-02-01 15:45:09 +0530

    indent custom configs

--- apis/fluentd/v1alpha1/plugins/custom/custom_types.go
+++ apis/fluentd/v1alpha1/plugins/custom/custom_types.go
@@ -1,8 +1,11 @@
 package custom
 
 import (
+	"bytes"
+	"fmt"
 	"github.com/fluent/fluent-operator/apis/fluentd/v1alpha1/plugins"
 	"github.com/fluent/fluent-operator/apis/fluentd/v1alpha1/plugins/params"
+	"strings"
 )
 
 type CustomPlugin struct {
@@ -15,6 +18,17 @@ func (c *CustomPlugin) Name() string {
 
 func (c *CustomPlugin) Params(_ plugins.SecretLoader) (*params.PluginStore, error) {
 	ps := params.NewPluginStore("")
-	ps.Content = c.Config
+	ps.Content = indentation(c.Config)
 	return ps, nil
+}
+
+func indentation(config string) string {
+	splits := strings.Split(config, "\n")
+	var buf bytes.Buffer
+	for _, split := range splits {
+		if split != "" {
+			buf.WriteString(fmt.Sprintf("  %s\n", split))
+		}
+	}
+	return buf.String()
 }
